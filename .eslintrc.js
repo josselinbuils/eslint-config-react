65425a9f4f9e55d33ab2ad85c4d31f8d5a19406d
Josselin BUILS <josselin.buils@gmail.com>
2020-11-20 23:38:03 +0100

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  extends: [
+    '@josselinbuils/eslint-config-typescript',
+    'airbnb/rules/react',
+    'airbnb/rules/react-a11y',
+    'airbnb/hooks',
+    'prettier/react',
+  ],
+  plugins: ['jsx-a11y', 'react'],
+  rules: {
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'jsx-a11y/anchor-is-valid': 'off',
+    'react/jsx-filename-extension': ['error', { extensions: ['.tsx'] }],
+    'react/jsx-props-no-spreading': 'off',
+    'react/no-danger': 'off',
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/sort-comp': 'off', // Does not manage arrow methods
+  },
+};
---
+++ README.md
@@ -0,0 +1,19 @@
+# eslint-config-react
+
+My own eslint rules.
+
+## Install
+
+```bash
+yarn add -D @josselinbuils/eslint-config-react
+```
+
+## Usage
+
+```js
+// .eslintrc.js
+
+module.exports = {
+  extends: '@josselinbuils/eslint-config-react',
+};
+```
